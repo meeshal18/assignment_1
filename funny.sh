75bead6bc7eb625026d429c6b2c89e86584eefdf
meeshalsahi18@gmail.com <meeshalsahi18@gmail.com>
2021-03-18 14:31:00 +0000

    question 3

--- funny.sh
+++ funny.sh
@@ -2,11 +2,11 @@
 
 #if no of arguments that our script was run with is greater then zero
 
-if ["$#" -eq 0]
+if [$# -ne 1];
 then 
 #print statment
 echo  "This is funny";
 else
 echo "This is Not funny";
-
+exit 1
 fi
