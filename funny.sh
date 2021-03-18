33c948823ff9ae2b5ae8776b01fe81239deed70f
meeshalsahi18@gmail.com <meeshalsahi18@gmail.com>
2021-03-18 13:12:01 +0000

    new file added

---
+++ funny.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#if no of arguments that our script was run with is greater then zero
+
+if ["$#" -eq 0]
+then 
+#print statment
+echo  "This is funny";
+else
+echo "This is Not funny";
+
+fi
